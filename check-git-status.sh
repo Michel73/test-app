7f79529d9af7bd3a29150e24899deb3c12d5b6cb
Michael Kopf <michael.kopf@sealsystems.de>
2018-01-23 14:05:52 +0100

    Wip.

--- check-git-status.sh
+++ check-git-status.sh
@@ -1,5 +1,5 @@
 changed=`git remote update >/dev/null 2>&1 && git status -uno | grep "have diverged" | wc -l`
-if [ $changed == 1 ]; then
+if [ $changed -eq 1 ]; then
   echo "There are remote git changes!"
-  exit 1; 
+  exit 1 
 fi 
